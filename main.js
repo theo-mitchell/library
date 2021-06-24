ce9a5260bdc1af41539b56cdeccdb07b690c90b8
theom95 <theodore.mitchell95@gmail.com>
2021-06-24 12:49:29 -0500

    Book addition implemented. Popup form halfway done

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 let myLibrary = [];
 
-function Book(author,title) {
+function Book(author, title, pages, read) {
     this.author = author,
     this.title = title;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary(event) {
     // Get all data and reset the form. 
     let author = bookForm.elements['author'].value;
     let title = bookForm.elements['title'].value;
+    let pages = bookForm.elements['pages'].value;
+    let read = bookForm.elements['read'].checked;
     // Form is reset manually because it is prevented from refreshing the page by onsubmit="return false" in HTML
     bookForm.reset();
     
-    let book = new Book(author, title)
+    let book = new Book(author, title, pages, read);
     myLibrary.push(book);
     console.log(myLibrary);
-    bookDisplay.textContent += book.title;
 
-    // for (obj of myLibrary) {
-    //     bookDisplay.textContent += obj.title;
-    // }
-
+    let bookDiv = document.createElement('div');
+    bookDiv.id = "book";
+    const text = `Author: ${book.author} \nTitle: ${book.title}\n Pages: ${book.pages}\n Read: ${book.read}`;
+    bookDiv.innerText = text;
+    bookDisplay.appendChild(bookDiv);
+    // bookDisplay.textContent += book.title;
 }
 
 
