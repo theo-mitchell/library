5c206cf35637da7cbe99df2dddbb16f3497ad529
theom95 <theodore.mitchell95@gmail.com>
2021-06-25 19:18:46 -0500

    Popup form+add book button functionality implemented. CSS impovements

--- main.js
+++ main.js
@@ -7,6 +7,25 @@ function Book(author, title, pages, read) {
     this.read = read;
 }
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function makeABookForDisplay(book){
+    let bookDiv = document.createElement('div');
+    bookDiv.id = "book";
+    const text = `Author: ${book.author} \nTitle: ${book.title}\n Pages: ${book.pages}\n Read: ${book.read}`;
+    bookDiv.innerText = text;
+    bookDiv.style.backgroundColor = getRandomColor();
+
+    return bookDiv;
+}
+
 function addBookToLibrary(event) {
     // Get all data and reset the form. 
     let author = bookForm.elements['author'].value;
@@ -15,20 +34,24 @@ function addBookToLibrary(event) {
     let read = bookForm.elements['read'].checked;
     // Form is reset manually because it is prevented from refreshing the page by onsubmit="return false" in HTML
     bookForm.reset();
-    
+    toggleForm();
+
     let book = new Book(author, title, pages, read);
     myLibrary.push(book);
     console.log(myLibrary);
 
-    let bookDiv = document.createElement('div');
-    bookDiv.id = "book";
-    const text = `Author: ${book.author} \nTitle: ${book.title}\n Pages: ${book.pages}\n Read: ${book.read}`;
-    bookDiv.innerText = text;
-    bookDisplay.appendChild(bookDiv);
-    // bookDisplay.textContent += book.title;
+    bookDisplay.appendChild(makeABookForDisplay(book));
+
 }
 
+function toggleForm(){
+    bookForm.classList.toggle("hidden");
+}
 
 const bookDisplay = document.getElementById('bookDisplay');
+
+const addBookButton = document.getElementById('openForm');
+addBookButton.addEventListener('click', toggleForm);
+
 const bookForm = document.getElementById('bookForm');
 bookForm.addEventListener('submit',addBookToLibrary);
